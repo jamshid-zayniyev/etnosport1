8f416acfb748981166d260ebef434d449cd4514e
Rahmonbek <83601261+Rahmonbek@users.noreply.github.com>
2022-02-24 12:32:39 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { BrowserRouter, Switch, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Dashboard from "./pages/Dashboard";
 import "./App.css";
 import Yangiliklar from "./pages/Yangiliklar";
@@ -9,20 +9,15 @@ export default class App extends Component {
     return (
       <div style={{ backgroundColor: "rgba(238, 243, 237, 0.857)" }}>
         <BrowserRouter>
-          <Dashboard />
-          {/* <Yangiliklar /> */}
-          {/* <Videolar /> */}
-          {/* <Switch>
-            <Route exact path="/">
-              <Dashboard />
-            </Route>
-            <Route path="/yangiliklar">
-              <Yangiliklar />
-            </Route>
-            <Route path="/videolavhalar">
-              <Videolar />
-            </Route>
-          </Switch> */}
+         
+          <Routes>
+            <Route to={<Dashboard />} exact path="/"/>
+              
+            <Route to={<Yangiliklar />} path="/yangiliklar"/>
+              
+            <Route to={<Videolar />} path="/videolavhalar"/>
+              
+          </Routes>
         </BrowserRouter>
       </div>
     );
