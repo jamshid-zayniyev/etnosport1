2298b33da5d5064a83e4b3c03c0cb9b736a39a42
Developer <62201299+Developer06092000@users.noreply.github.com>
2022-03-28 10:45:39 +0500

    Update Viloyatlar.js

--- src/pages/Viloyatlar.js
+++ src/pages/Viloyatlar.js
@@ -4,26 +4,51 @@ import Footer from "./Footer";
 import style from "../css/Dashboard.module.css";
 import styles from "../css/Gallery.module.css";
 import etnosport8 from "../img/etnoimg8.jpg";
+import school from "../img/gerb.png";
 import Dashnews from "./Dashnews";
 export default class Viloyatlar extends Component {
-  state={
-    nameR:'',
-    id:""
-  }
+  state = {
+    nameR: "",
+    id: "",
+    loader: true,
+  };
   componentDidMount() {
-    setInterval(()=>{
-       if(window.location.href.split('/')[window.location.href.split('/').length-2]!==this.state.id){
+    setInterval(() => {
+      if (
+        window.location.href.split("/")[
+          window.location.href.split("/").length - 2
+        ] !== this.state.id
+      ) {
         this.setState({
-          nameR:window.location.href.split('/')[window.location.href.split('/').length-1].replaceAll('%20', " "),
-          id:window.location.href.split('/')[window.location.href.split('/').length-2]
-        })
-       }
-    }, 500)
-     }
+          nameR: window.location.href
+            .split("/")
+            [window.location.href.split("/").length - 1].replaceAll("%20", " "),
+          id: window.location.href.split("/")[
+            window.location.href.split("/").length - 2
+          ],
+          loader: true,
+        });
+        setTimeout(() => {
+          this.setState({
+            loader: false,
+          });
+        }, 1000);
+      }
+    }, 500);
+  }
   render() {
-    
     return (
       <div>
+        {" "}
+        {this.state.loader ? (
+          <div className="loaderG">
+            <div className="befG">
+              <img src={school} alt="..." />
+            </div>
+          </div>
+        ) : (
+          ""
+        )}
         <Header />
         <div className={style.head}>
           <img src={etnosport8}></img>
@@ -31,9 +56,8 @@ export default class Viloyatlar extends Component {
             {this.state.nameR} xotin-qizlar sport tadbirlari
           </div>
 
-          <Dashnews id={this.state.id}/>
+          <Dashnews id={this.state.id} />
         </div>
-
         <Footer />
       </div>
     );
